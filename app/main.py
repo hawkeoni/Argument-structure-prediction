6432c0b1f05bf9904c3ba4a06eb80d6731969e14
ilya <iliyadimov@icloud.com>
2020-10-27 18:20:13 +0300

    Intermediate app state

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from allennlp.predictors import Predictor
+from flask import Flask, render_template, request
 
+import src
 
 app = Flask(__name__, template_folder="templates")
+predictor = Predictor.from_path("model.tar.gz")
 
-
-@app.route("/topic_claim")
+@app.route("/topic_claim", methods=["get", "post"])
 def topic_claim():
-    return render_template("topic_claim.html")
+    if request.method == "GET":
+        return render_template("topic_claim.html")
+    elif request.method == "POST":
+        evidence = request.form.get('evidence')  # запрос к данным формы
+        topic = request.form.get('topic')
+        return str(predictor.predict_texts(sentence=evidence, topic=topic))
+
 
 
 if __name__ == "__main__":
