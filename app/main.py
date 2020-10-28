ce76e60d884bc736571db235605ca3c49cc7e799
ilya <iliyadimov@icloud.com>
2020-10-28 14:36:09 +0300

    Add dockerfile stub

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from allennlp.predictors import Predictor
 from flask import Flask, render_template, request
 
-import src
+# import src
 
 app = Flask(__name__, template_folder="templates")
-predictor = Predictor.from_path("model.tar.gz")
+# predictor = Predictor.from_path("model.tar.gz")
+
 
 @app.route("/topic_claim", methods=["get", "post"])
 def topic_claim():
@@ -13,9 +14,9 @@ def topic_claim():
     elif request.method == "POST":
         evidence = request.form.get('evidence')  # запрос к данным формы
         topic = request.form.get('topic')
-        return str(predictor.predict_texts(sentence=evidence, topic=topic))
-
+        return topic + " " + evidence
+        # return str(predictor.predict_texts(sentence=evidence, topic=topic))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
