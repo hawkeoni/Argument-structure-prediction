44afcb0d4c09648544ae5657cac28d232616084f
Ilya Dimov <iliyadimov@icloud.com>
2021-04-26 18:18:44 +0300

    Make unionized data processing for es & terra

--- argmining/data_processing/process_ibm_evidence_search.py
+++ argmining/data_processing/process_ibm_evidence_search.py
@@ -1,32 +1,53 @@
+import json
 from pathlib import Path
+from typing import Union
 
 import typer
 import pandas as pd
+from tqdm import tqdm
 
 
 def remove_ref(s: str) -> str:
     return s.replace("[REF]", "")
 
+def get_label(label: Union[int, str]):
+    if isinstance(label, str):
+        return label
+    return {0: "Neutral", 1: "Evidence"}[label]
 
-def process_ibm_evidencesearch_df(df: pd.DataFrame) -> pd.DataFrame:
+def process_ibm_evidencesearch_df(df: pd.DataFrame, filename: str) -> pd.DataFrame:
     """
     Remove refs, improve label.
     """
-    df["claim"] = df["topic"].apply(remove_ref)
-    df["evidence"] = df["candidate"].apply(remove_ref)
-    df["label"] = df["label"].apply(lambda x: "Evidence" if x == 1 else "Neutral")
-    columns_to_keep = ["claim", "evidence", "label"]
-    return df[columns_to_keep]
+    f = open(filename, "w")
+    for rownum, row in tqdm(df.iterrows()):
+        d = {}
+        d["sentence1"] = remove_ref(row["topic"])
+        d["sentence2"] = remove_ref(row["candidate"])
+        d["gold_label"] = get_label(row["label"])
+        f.write(json.dumps(d))
+        f.write("\n")
+    f.close()
+
 
 
 def main(evidence_search_folder: Path):
+    save_path = Path.cwd() / "datasets"
+    # English
+    print("Eng train")
     train_df = pd.read_csv(evidence_search_folder / "train.csv")
+    print("Eng test")
     test_df = pd.read_csv(evidence_search_folder / "test.csv")
-    train_df = process_ibm_evidencesearch_df(train_df)
-    test_df = process_ibm_evidencesearch_df(test_df)
-    train_df.to_csv(Path.cwd() / "datasets/ibm_es_train.csv", index=False)
-    test_df.to_csv(Path.cwd() / "datasets/ibm_es_test.csv", index=False)
-
+    process_ibm_evidencesearch_df(train_df, save_path / "ibm_es_train.jsonl")
+    process_ibm_evidencesearch_df(test_df, save_path / "ibm_es_test.jsonl")
+    # Russian
+    print("Ru train")
+    train_df = pd.read_csv(evidence_search_folder / "ibm_es_rutrain.csv")
+    print("Ru test")
+    test_df = pd.read_csv(evidence_search_folder / "test.csv")
+    test_df = pd.read_csv(evidence_search_folder / "ibm_es_rutest.csv")
+    process_ibm_evidencesearch_df(train_df, save_path / "ibm_es_rutrain.jsonl")
+    process_ibm_evidencesearch_df(test_df, save_path / "ibm_es_rutest.jsonl")
 
 if __name__ == "__main__":
     typer.run(main)
--- argmining/data_processing/process_terra.py
+++ argmining/data_processing/process_terra.py
@@ -4,25 +4,29 @@ from collections import defaultdict
 
 import typer
 import pandas as pd
+from tqdm import tqdm
 
 
-def create_df_from_jsonl(file: Path):
-    columns = ["claim", "evidence", "label"]
+def transform_jsonl(file: Path, outfile: Path):
+    f = open(outfile, "w")
     label_mapping = {"entailment": "Evidence", "not_entailment": "Neutral"}
-    df_dict = defaultdict(list)
-    for line in file.open():
-        example = json.loads(line)
-        df_dict["claim"].append(example["hypothesis"])
-        df_dict["evidence"].append(example["premise"])
-        df_dict["label"].append(label_mapping.get(example.get("label")))
-    return pd.DataFrame(df_dict, columns=columns)
+    for line in tqdm(open(file)):
+        d = json.loads(line)
+        d["sentence1"] = d["premise"]
+        d["sentence2"] = d["hypothesis"]
+        d["gold_label"] = label_mapping[d["label"]]
+        f.write(json.dumps(d, ensure_ascii=False))
+        f.write("\n")
+    f.close()
+
+
+
 
 
 def main(terra_folder: Path):
-    files = terra_folder.glob("*jsonl")
+    files = [terra_folder / "train.jsonl", terra_folder / "val.jsonl"]
     for file in files:
-        dump_path = Path.cwd() / "datasets" / ("terra_" + file.name.replace("jsonl", "csv"))
-        create_df_from_jsonl(file).to_csv(dump_path, index=False)
+        transform_jsonl(file, Path.cwd() / "datasets" / ("terra_" + file.name))
 
 
 if __name__ == "__main__":
